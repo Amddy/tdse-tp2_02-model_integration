e51186677c96ebfb9c01003b65c55aa2529f8500
Amddy <azambranom@fi.uba.ar>
2025-04-23 20:17:46 -0300

    second Commit

--- app/inc/board.h
+++ app/inc/board.h
@@ -64,6 +64,12 @@ extern "C" {
 #define BTN_A_PRESSED	GPIO_PIN_RESET
 #define BTN_A_HOVER		GPIO_PIN_SET
 
+#define BTN_B_PIN		D7_Pin
+#define BTN_B_PORT		D7_GPIO_Port
+#define BTN_B_PRESSED	GPIO_PIN_RESET
+#define BTN_B_HOVER		GPIO_PIN_SET
+
+
 #define LED_A_PIN		LD2_Pin
 #define LED_A_PORT		LD2_GPIO_Port
 #define LED_A_ON		GPIO_PIN_SET
